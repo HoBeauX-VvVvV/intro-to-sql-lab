67c542abdf542597c932028964720f8740d18406
BeauX <fire-fly_7@hotmail.com>
2024-11-23 17:07:36 -0800

    clue 5 commit

--- clues.sql
+++ clues.sql
@@ -51,6 +51,16 @@ WHERE countries.name = 'San Marino'
 
 -- Write SQL query here
 
+SELECT cities.name, countries.name AS country
+FROM cities
+JOIN countries ON cities.countrycode = countries.code
+WHERE countries.continent = 'South America' 
+  AND cities.name LIKE 'Serra';
+ 
+--SELECT countries.name AS country
+--FROM cities
+--JOIN countries ON cities.countrycode = countries.code
+--WHERE cities.name = 'Serra';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and 
