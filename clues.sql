8b2d78187c4951497e3c778e08d390cb0bcfc61d
BeauX <fire-fly_7@hotmail.com>
2024-11-23 17:19:09 -0800

    clue 7 commit

--- clues.sql
+++ clues.sql
@@ -90,3 +90,8 @@ WHERE countries.name = 'Brazil';
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her 
 --at the gates with bells on.
+
+SELECT cities.name AS City, countries.name AS Country
+FROM cities
+JOIN countries ON cities.countrycode = countries.code
+WHERE cities.population = 91084;
